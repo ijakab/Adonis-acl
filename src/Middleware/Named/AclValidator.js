8524b415a3380147ce2fa0be57b8141b998e9ce5
Ivan Jakab <ivan.jakab@gauss.hr>
2018-08-14 11:33:41 +0200

    fixed middleware bug

--- src/Middleware/Named/AclValidator.js
+++ src/Middleware/Named/AclValidator.js
@@ -4,13 +4,13 @@ const parser = use('Adonis/Acl/Parser')
 
 class AclValidator {
   async handle (ctx, next, aclParams) {
-    // call next to advance the request
+      if(!ctx.user) ctx.user = await ctx.auth.getUser()
       let serviceStr = parser.makeServiceStr(aclParams, ctx.request.all())
       let operation_info = parser.operationInfo(aclParams[0])
 
       let passes = true
       for(let requirement of operation_info.requirements) { //todo: ABSOLUTELY refactor this
-        if(!(await AclService[operation_info.operationName](serviceStr, requirement, user.id))) {
+        if(!(await AclService[operation_info.operationName](serviceStr, requirement, ctx.user.id)).aclResult) {
           passes = false
           break
         }
@@ -18,7 +18,7 @@ class AclValidator {
       if(passes)
         await next()
       else
-        return
+        return ctx.response.unauthorized('user.noPermission')
   }
 }
 
